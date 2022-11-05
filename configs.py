cdcdf75422d696585eda740b5b6b9682033b8237
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-11-05 12:36:18 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ API_ID = int(environ.get("API_ID", 18))
 API_HASH = environ.get("API_HASH", None)
 OWNER_ID = int(environ.get("OWNER_ID", None))
 PREFIX_HANDLER = environ.get("PREFIX_HANDLER", default="/").split()
-group = environ("CHANNEL", default="-1001625864096")
+group = environ("CHANNEL", default="1752851548 1747117888 1735333104")
 CHANNEL = [int(i) for i in group.split(" ")]
 sudo = environ("SUDO", default="")
 SUDO = [int(i) for i in sudo.split(" ")]
