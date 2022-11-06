2eb6b6d0853528ac711e4c1ab1c9edfcedeb90a7
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-11-06 23:09:24 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 API_ID = int(getenv("API_ID", 18))
 API_HASH = getenv("API_HASH", None)
 OWNER_ID = int(getenv("OWNER_ID", None))
-PREFIX_HANDLER = getenv("PREFIX_HANDLER", default="/").split()
+PREFIX_HANDLER = getenv("PREFIX_HANDLER", default="/ ! $").split()
 group = getenv("CHANNEL", default="1747117888")
 CHANNEL = [int(i) for i in group.split(" ")]
 sudo = getenv("SUDO", default="")
