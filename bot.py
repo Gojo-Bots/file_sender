be6b3c6faf38b85cea91c2829d281bf4ad50b1e3
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-11-07 12:44:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -176,7 +176,7 @@ async def forwardto(_, m: Message):
     except Exception as e:
         return await m.reply_text(f"Got an error:\n{e}")
 
-@psy.on_message(filters.private & filters.photo)
+@psy.on_message(filters.photo)
 async def forwarder(_, m: Message):
     if m.chat.id == BOT_ID:
         if not bool(m.photo):
